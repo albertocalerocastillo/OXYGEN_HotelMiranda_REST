59920552207d404ecdff82c76f866202dee67f30
albertocalerocastillo <albertocalerocastillo@gmail.com>
2025-03-04 20:37:39 +0100

    ADD: Connection MySQL database, db.ts and seed.ts

--- seed.ts
+++ seed.ts
@@ -1,124 +1,139 @@
+import mysql from 'mysql2/promise';
+import { RowDataPacket } from 'mysql2';
+import { connect } from './db';
 import { faker } from '@faker-js/faker';
-import mongoose from 'mongoose';
-import { RoomModel } from './src/models/RoomModel';
-import { BookingModel } from './src/models/BookingModel';
-import { UserModel } from './src/models/UserModel';
-import { ContactModel } from './src/models/ContactModel';
-import { connectDB } from './database';
-import 'dotenv/config';
-import bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
 
 async function main() {
-  await connectDB();
+    let connection: mysql.PoolConnection | null = null;
 
-  async function generateContacts() {
-    const date = faker.date.anytime();
-    const name = faker.person.fullName();
-    const email = faker.internet.email();
-    const phone = faker.phone.number();
-    const comment = faker.lorem.paragraph();
-    const archived = faker.datatype.boolean();
+    try {
+        connection = await connect();
 
-    const contact = new ContactModel({
-      date,
-      customer: name,
-      email,
-      phone,
-      subject: faker.lorem.sentence(),
-      comment,
-      archived,
-    });
+        if (connection) {
+            async function generateUsers() {
+                for (let i = 0; i < 10; i++) {
+                    const id = uuidv4();
+                    const name = faker.person.fullName();
+                    const email = faker.internet.email();
+                    const joinDate = faker.date.past().toISOString().split('T')[0];
+                    const jobDesk = faker.person.jobTitle();
+                    const contact = faker.phone.number();
+                    const status = faker.helpers.arrayElement(['ACTIVE', 'INACTIVE']);
+                    const profilePhoto = faker.image.avatar();
+                    const password = faker.internet.password();
 
-    await contact.save();
-  }
+                    await connection!.execute(
+                        'INSERT INTO users (id, name, email, joinDate, jobDesk, contact, status, profilePhoto, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                        [id, name, email, joinDate, jobDesk, contact, status, profilePhoto, password]
+                    );
+                }
+                console.log('Usuarios generados correctamente');
+            }
 
-  async function generateBookings() {
-    const guest = faker.person.fullName();
-    const orderDate = faker.date.anytime();
-    const checkInDate = faker.date.future();
-    const checkInTime = faker.date.recent().toLocaleTimeString();
-    const checkOutDate = faker.date.future();
-    const checkOutTime = faker.date.recent().toLocaleTimeString();
-    const specialRequest = faker.lorem.sentence();
-    const status = faker.helpers.arrayElement(['Booked', 'Refund', 'Pending', 'Canceled']);
-    const specialRequestType = faker.lorem.word();
+            async function generateContacts() {
+                for (let i = 0; i < 10; i++) {
+                    const id = uuidv4();
+                    const date = faker.date.anytime().toISOString().split('T')[0];
+                    const customer = faker.person.fullName();
+                    const email = faker.internet.email();
+                    const phone = faker.phone.number();
+                    const subject = faker.lorem.sentence();
+                    const comment = faker.lorem.paragraph();
+                    const archived = faker.datatype.boolean();
 
-    const booking = new BookingModel({
-      guest,
-      orderDate,
-      checkInDate,
-      checkInTime,
-      checkOutDate,
-      checkOutTime,
-      specialRequest,
-      room: null,
-      status,
-      specialRequestType,
-    });
+                    await connection!.execute(
+                        'INSERT INTO contacts (id, date, customer, email, phone, subject, comment, archived) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                        [id, date, customer, email, phone, subject, comment, archived]
+                    );
+                }
+                console.log('Contactos generados correctamente');
+            }
 
-    await booking.save();
-  }
+            async function generateRooms() {
+                for (let i = 0; i < 10; i++) {
+                    const id = uuidv4();
+                    const name = faker.lorem.sentence();
+                    const type = faker.lorem.word();
+                    const number = faker.number.int({ min: 1, max: 9999 }).toString();
+                    const amenities = faker.lorem.sentence();
+                    const price = faker.number.int({ min: 50, max: 500 });
+                    const discount = faker.number.int({ min: 0, max: 50 });
+                    const offerPrice = faker.number.int({ min: 25, max: 250 }).toString();
+                    const photos = faker.image.url();
+                    const status = faker.helpers.arrayElement(['Available', 'Booked']);
+                    const description = faker.lorem.paragraph();
+                    const offer = faker.lorem.sentence();
+                    const cancellation = faker.lorem.sentence();
 
-  async function generateRooms() {
-    const photo = faker.image.url();
-    const number = faker.number.int({ min: 1, max: 9999 }).toString();
-    const name = faker.lorem.sentence();
-    const type = faker.lorem.word();
-    const amenities = faker.lorem.sentence();
-    const price = faker.number.int();
-    const offerPrice = faker.number.int();
-    const status = faker.helpers.arrayElement(['Available', 'Booked']);
-    const description = faker.lorem.paragraph();
-    const capacity = faker.number.int();
+                    await connection!.execute(
+                        'INSERT INTO rooms (id, name, type, number, amenities, price, discount, offerPrice, photos, status, description, offer, cancellation) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                        [id, name, type, number, amenities, price, discount, offerPrice, photos, status, description, offer, cancellation]
+                    );
+                }
+                console.log('Habitaciones generadas correctamente');
+                console.log('generateRooms completado');
+            }
 
-    const room = new RoomModel({
-      photo,
-      number,
-      name,
-      type,
-      amenities,
-      price,
-      offerPrice,
-      status,
-      description,
-      capacity,
-    });
+            async function generateBookings() {
+              console.log('generateBookings iniciado');
+              console.log('Consultando IDs de habitaciones...');
+              const [rows] = await connection!.execute('SELECT id FROM rooms');
+              console.log('Resultado de la consulta:', rows);
+          
+              const roomIds: string[] = [];
+              if (Array.isArray(rows)) {
+                  for (const row of rows) {
+                      if (typeof row === 'object' && row.hasOwnProperty('id')) {
+                          const rowData = row as RowDataPacket;
+                          roomIds.push(rowData.id);
+                      }
+                  }
+              }
+          
+              if (roomIds.length > 0) {
+                  console.log('IDs de habitaciones:', roomIds);
+          
+                  for (let i = 0; i < 10; i++) {
+                      const id = uuidv4();
+                      const guest = faker.person.fullName();
+                      const orderDate = faker.date.anytime().toISOString().split('T')[0];
+                      const checkInDate = faker.date.future().toISOString().split('T')[0];
+                      const checkInTime = faker.date.recent().toLocaleTimeString();
+                      const checkOutDate = faker.date.future().toISOString().split('T')[0];
+                      const checkOutTime = faker.date.recent().toLocaleTimeString();
+                      const specialRequest = faker.lorem.sentence();
+                      const roomId = faker.helpers.arrayElement(roomIds);
+                      const status = faker.helpers.arrayElement(['Booked', 'Refund', 'Pending', 'Canceled']);
+                      const specialRequestType = faker.lorem.word();
+          
+                      await connection!.execute(
+                          'INSERT INTO bookings (id, guest, orderDate, checkInDate, checkInTime, checkOutDate, checkOutTime, specialRequest, roomId, status, specialRequestType) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                          [id, guest, orderDate, checkInDate, checkInTime, checkOutDate, checkOutTime, specialRequest, roomId, status, specialRequestType]
+                      );
+                  }
+                  console.log('Reservas generadas correctamente');
+              } else {
+                  console.log('No se encontraron habitaciones para generar reservas.');
+              }
+              console.log('generateBookings completado');
+          }
+            await generateUsers();
+            await generateContacts();
+            await generateRooms();
+            await generateBookings();
 
-    await room.save();
-  }
-
-  async function generateUsers() {
-    const name = faker.person.fullName();
-    const email = faker.internet.email();
-    const joinDate = faker.date.past();
-    const jobDesk = faker.person.jobTitle();
-    const contact = faker.phone.number();
-    const status = faker.helpers.arrayElement(['ACTIVE', 'INACTIVE']);
-    const profilePhoto = faker.image.avatar();
-    const password = 'admin';
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = new UserModel({
-      name,
-      email,
-      joinDate,
-      jobDesk,
-      contact,
-      status,
-      profilePhoto,
-      password: hashedPassword,
-    });
-
-    await user.save();
-  }
-
-  for (let i = 0; i < 1; i++) {
-    await generateContacts();
-    await generateBookings();
-    await generateRooms();
-    await generateUsers();
-  }
+            console.log('Seed completado');
+        } else {
+            console.error('No se pudo establecer la conexión a la base de datos');
+        }
+    } catch (error) {
+        console.error('Error en el seed:', error);
+    } finally {
+        if (connection) {
+            connection.end();
+        }
+    }
 }
 
 main();
